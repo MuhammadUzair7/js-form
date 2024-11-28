228394922f4692338f7988e3ed3f65dca01a07d7
muhammaduzair4212 <muhammaduzair12571@gmail.com>
2024-11-28 12:51:58 +0500

    complete js form

---
+++ app.js
@@ -0,0 +1,28 @@
+// function getDetail() {
+//     var city = document.getElementById("city").value;
+//     var fname = document.getElementById("fname").value;
+//     var lname = document.getElementById("lname").value;
+//     var email = document.getElementById("email").value;
+//     var phone = document.getElementById("phone").value;
+//     var cnic = document.getElementById("cnic").value;
+//     var bDate = document.getElementById("bDate").value;
+//     var gender = document.getElementById("gender").value;
+//     var address = document.getElementById("address").value;
+   
+//     var tableID = document.getElementById("tableID").innerHTML +=
+//         `
+//     <tr>
+//         <td class="border text-nowrap">${city}</td>
+//         <td class="border text-nowrap">${fname}</td>
+//         <td class="border text-nowrap">${phone}</td>
+//         <td class="border text-nowrap">${email}</td>
+//         <td class="border text-nowrap">${cnic}</td>
+//         <td class="border text-nowrap">${bDate}</td>
+//         <td class="border text-nowrap">${gender}</td>
+//         <td class="border text-nowrap">${lname}</td>
+//         <td class="border text-nowrap">${address}</td>
+//         <td class="border text-nowrap">${tableID}</td>
+        
+//     </tr>
+//     `;
+// }
